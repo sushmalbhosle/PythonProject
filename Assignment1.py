08aecebe4fcb4cfed967d68645f917f257884725
Sushma <sushmalbhoslemitali66@gmail.com>
2025-12-13 17:12:18 +0530

    new Assignment1

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter the firsrt number "))
+num2 = float(input("Enter the second number "))
+
+
+
+
+Addition = num1 + num2
+print(Addition)
+Subtraction = num1-num2
+print(Subtraction)
+Multiplication = num1 * num2
+print(Multiplication)
+Division = num1 / num2
+print(Division)
