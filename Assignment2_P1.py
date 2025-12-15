765b6bc6b08617a0552ad5be1c66b522ccc08707
Sushma <sushmalbhoslemitali66@gmail.com>
2025-12-15 13:47:07 +0530

    new Assignment2-p2

---
+++ Assignment2_P1.py
@@ -0,0 +1,8 @@
+# Takes an integer input from the user.
+# Checks whether the number is even or odd using an if-else statement.
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
